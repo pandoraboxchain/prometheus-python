2b73a18e3a75a8de617a557d2a000ebb4ae2ce06
Julian Konchunas <konchunas@gmail.com>
2018-05-19 02:44:59 +0300

    fix block deserialization

--- chain/block.py
+++ chain/block.py
@@ -11,7 +11,9 @@ class Block():
         hash_count = struct.unpack_from("h", raw_data, 4)[0]
         self.prev_hashes = []
         for i in range(0, hash_count):
-            prev_hash = struct.unpack_from("32s", raw_data, 6 + 32 * i)[0]
+            hash_start = 6 + 32 * i
+            hash_end = hash_start + 32
+            prev_hash = raw_data[hash_start : hash_end]
             self.prev_hashes.append(prev_hash)
         offset = 6 + 32 * hash_count
         randoms_count = struct.unpack_from("h", raw_data, offset)[0]
@@ -23,5 +25,8 @@ class Block():
         raw_block += struct.pack("h", len(self.prev_hashes))
         for prev_hash in self.prev_hashes:
             raw_block += prev_hash
-        raw_block += struct.pack("h%sh" % len(self.randoms), len(self.randoms), *self.randoms)
+        if hasattr(self, 'randoms') and len(self.randoms) > 0:
+            raw_block += struct.pack("h%sh" % len(self.randoms), len(self.randoms), *self.randoms)
+        else:
+            raw_block += struct.pack("h", 0)
         return raw_block
--- chain/signed_block.py
+++ chain/signed_block.py
@@ -5,17 +5,13 @@ from chain.block import Block
 class SignedBlock():
 
     def parse(self, raw_data):
-        signature_len = struct.unpack_from("h", raw_data)[0]
-        self.signature = struct.unpack_from("%sh" % signature_len, raw_data, 2)[0]
-        block_length = struct.unpack_from("h", raw_data, 2 + signature_len)[0]
-        raw_block = struct.unpack_from("%sh" % block_length, raw_data, 2 + signature_len + 2)[0]
+        self.signature = int.from_bytes(raw_data[0:128], byteorder='big')
+        block_length = struct.unpack_from("h", raw_data, 128)[0]
+        raw_block = raw_data[130:130+block_length]
         self.block = Block()
         self.block.parse(raw_block)
 
     def pack(self):
-        #signature_len = hex(self.signature)
-        #raw_signed_block = struct.pack("h", signature_len)
-        #raw_signed_block += struct.pack("%sh" % signature_len, self.signature)
         raw_block = self.block.pack()
         raw_signed_block = self.signature.to_bytes(128, byteorder='big')
         raw_signed_block += struct.pack("h", len(raw_block))
@@ -30,4 +26,4 @@ class SignedBlock():
 
     def verify_signature(self, pubkey):
         block_hash = self.block.get_hash().digest()
-        pubkey.verify(block_hash, self.signature)
+        pubkey.verify(block_hash, (self.signature,))
