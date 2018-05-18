bb08fc1158e50dbd446c22f1829eaa7864be246b
Julian Konchunas <konchunas@gmail.com>
2018-05-18 17:41:57 +0300

    Block parsing and packing

--- chain/block.py
+++ chain/block.py
@@ -1,5 +1,5 @@
-import struct
 from Crypto.Hash import SHA256
+import struct
 
 class Block():
 
@@ -10,15 +10,28 @@ class Block():
 
     def set_raw_data(self, raw_data):
         self.raw_data = raw_data
-        if hasattr(self, 'hash')
+        if hasattr(self, 'hash'):
             del self.hash
 
     def parse(self):
-        self.timestamp = struct.unpack('<L', self.raw_data)[0]
+        self.timestamp = struct.unpack_from("I", self.raw_data)
+        self.prev_hash = struct.unpack_from("32s", self.raw_data, 4)
+        randoms_count = struct.unpack_from("h", self.raw_data, 36)[0]
+        if randoms_count > 0:
+            self.randoms = struct.unpack_from("%sh" % randoms_count, self.raw_data, 38)
+
+    def pack(self):
+        block_format = "I32sh%sh" % len(self.randoms)
+        block_struct = struct.Struct(block_format)
+        return block_struct.pack(self.timestamp,
+            self.prev_hash.digest(),
+            len(self.randoms),
+            *self.randoms)
+
 
     def set_signature(self, signature):
         self.signature = signature
 
-    def verify_signature(self, pubkey)
+    def verify_signature(self, pubkey):
         self.get_hash().digest()
         public_key.verify(self.get_hash(), self.signature)
---
+++ tests/block.py
@@ -0,0 +1,18 @@
+import unittest
+#from ...chain.block import Block #TODO fix that import
+from Crypto.Hash import SHA256
+
+class TestBlock(unittest.TestCase):
+
+    def test_pack_parse(self):
+        original_block = Block();
+        original_block.timestamp = 2344
+        original_block.prev_hash = SHA256.new(b"323423")
+        original_block.randoms = [1,2,3,4,5]
+        
+        raw = original_block.pack();
+        restored = Block()
+        restored.set_raw_data(raw)
+        restored.parse();
+
+        self.assertEqual(original_block, restored)
