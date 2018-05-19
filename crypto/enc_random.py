5c340c6e9051e151cd3338e80eac734da495b791
Andrey Sobol <sobol@pandora.foundation>
2018-05-19 02:47:41 +0200

    enc_random

---
+++ crypto/enc_random.py
@@ -0,0 +1,12 @@
+import struct
+from Crypto.PublicKey import RSA
+from Crypto import Random
+
+def enc_part_random(era_hash):
+    random_generator = Random.new().read
+    key = RSA.generate(1024, random_generator)
+    part_random = [random.getrandbits(8) for item in range(32)]
+    part_random_pack = struct.pack("%sh" % len(part_random), *part_random)
+    ed = part_random_pack+era_hash
+    enc_data = key.publickey().encrypt(ed, 32)
+    return (enc_data, key)
