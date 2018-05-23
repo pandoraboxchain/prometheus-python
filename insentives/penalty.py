ce815327b90ee75f5a05dc49e7b73f6c542302dd
Andrey Sobol <sobol@pandora.foundation>
2018-05-23 16:22:57 +0200

    PenaltyNotIncludingGossip

--- insentives/penalty.py
+++ insentives/penalty.py
@@ -12,6 +12,16 @@ class PenaltyGossipAboutMe():
     def get_penalty(self, dag, block_obj, block_sub):
         return block_sub.gossips_about_block().get_size_of_negative_gosssip()*1
 
+class PenaltyNotIncludingGossip():
+
+    def get_penalty(self, dag, block_obj, block_sub):
+        gossips = block_obj.gossips_about_block()
+        counter = 0
+        for g in gossips:
+            if not (g.block_number > block_sub):
+                counter += 1
+        return counter*4
+
 class PanaltyByBlock():
     pass
 
