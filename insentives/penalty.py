9832515e46354fc6c2fd5bbf450783da85364245
Andrey Sobol <sobol@pandora.foundation>
2018-05-23 16:06:02 +0200

    PenaltyGossipAboutMe

--- insentives/penalty.py
+++ insentives/penalty.py
@@ -7,6 +7,10 @@ class PenaltyPrevBlock():
         if block_obj==None:
             return 2
 
+class PenaltyGossipAboutMe():
+
+    def get_penalty(self, dag, block_obj, block_sub):
+        return block_sub.gossips_about_block().get_size_of_negative_gosssip()*1
 
 class PanaltyByBlock():
     pass
