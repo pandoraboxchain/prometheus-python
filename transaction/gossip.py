578e44c9e4f7910f98c9db425875bcad917e2688
Andrey Sobol <sobol@pandora.foundation>
2018-05-28 21:15:21 +0300

    template for gossip

---
+++ transaction/gossip.py
@@ -0,0 +1,10 @@
+class Gossip():
+    pass
+
+class NegativeGossip(Gossip):
+    pass
+    #validator, signature_validator, number_of_block
+
+class PositiveGossip(Gossip):
+    pass
+    #validator, signature_validator, hash_block
