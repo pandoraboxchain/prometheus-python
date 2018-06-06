bfc19a746538fabc3f3c543531253cead3f169bb
Julian Konchunas <konchunas@gmail.com>
2018-06-06 17:22:43 +0300

    switch to os.urandom for commit

--- crypto/enc_random.py
+++ crypto/enc_random.py
@@ -7,9 +7,8 @@ from Crypto import Random
 def enc_part_random(era_hash):
     random_generator = Random.new().read
     key = RSA.generate(1024, random_generator)
-    part_random = [random.getrandbits(8) for item in range(32)]
-    part_random_pack = struct.pack("%sB" % len(part_random), *part_random)
-    ed = part_random_pack+era_hash
+    random = os.urandom(32)
+    ed = random+era_hash
     enc_data = key.publickey().encrypt(ed, 32)[0]
     return (enc_data, key)
 
