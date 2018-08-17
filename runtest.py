568c630a8bbc75f888e88f48d0fbbf478e848edd
Strider <striderbox@gmail.com>
2018-08-17 17:46:24 +0300

    - permissions tests fix

--- runtest.py
+++ runtest.py
@@ -1,3 +1,14 @@
+import unittest
+
+from tests.block import *
+from tests.transaction import *
+from tests.stake_transaction import *
+from tests.enc_dec import *
+from tests.epoch import *
+from tests.secret_sharing import *
+from tests.dag import *
+from tests.merger import *
+from tests.permissions import *
 from tests.random import *
 
 unittest.main()
