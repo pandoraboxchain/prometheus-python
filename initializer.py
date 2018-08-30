028a1d048876df5b2844487ce4795ffc40bfa237
Julian Konchunas <konchunas@gmail.com>
2018-08-30 17:10:12 +0300

    Save dag visualization on exit even if error occured

--- initializer.py
+++ initializer.py
@@ -11,66 +11,65 @@ import datetime
 import time
 import asyncio
 import logging
-import signal
 import sys
 import importlib
 
 #you can set node to visualize its DAG as soon as Ctrl-C pressed
-node_to_visualize = None
-def signal_handler(sig, frame):
+def save_dag_to_graphviz(dag_to_visualize):
     graphviz_import = importlib.util.find_spec("graphviz")
     graphviz_lib_installed = graphviz_import is not None
-    if node_to_visualize and graphviz_lib_installed:
+    if graphviz_lib_installed:
         from visualization.dag_visualizer import DagVisualizer
-        DagVisualizer.visualize(node_to_visualize.dag)
-    sys.exit(0)
+        DagVisualizer.visualize(dag_to_visualize)
 
 class Initializer(): 
 
     def __init__(self):
+        node_to_visualize_after_exit = None
+        try:
+            # set up logging to file - see previous section for more details
+            logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)-6s %(name)-6s %(message)s')
+
+            genesis_creation_time = int(datetime.datetime.now().timestamp() - BLOCK_TIME) #so we start right from the first block
+
+            private_keys = BlockSigners()
+
+            network = NodeApi()
+
+            tasks = []
+
+            logger = logging.getLogger("Announce")
+            announcer = AnnouncerNode(genesis_creation_time, logger)
+            tasks.append(announcer.run())
+            
+            for i in range(0, 19):
+                behaviour = Behaviour()
+                logger = logging.getLogger("Node " + str(i))
+                # uncomment the following line to enable logging only on specific node
+                # if i != 13: logger.setLevel(logging.CRITICAL)
+                node = Node(genesis_creation_time, i, network, logger, private_keys.block_signers[i], behaviour)
+
+                if i == 0: node_to_visualize_after_exit = node
+                network.register_node(node)
+                tasks.append(node.run())
+
+            for i in range(19, 20):
+                behaviour = Behaviour()
+                behaviour.wants_to_hold_stake = True
+                behaviour.epoch_to_release_stake = 2
+                logger = logging.getLogger("Node " + str(i))
+                keyless_node = Node(genesis_creation_time, i, network, logger, None, behaviour)
+                network.register_node(keyless_node)
+                tasks.append(keyless_node.run())
+
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(asyncio.gather(*tasks))       
+            loop.close()
         
-        # set up logging to file - see previous section for more details
-        logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)-6s %(name)-6s %(message)s')
+        finally:
+            if node_to_visualize_after_exit:
+                save_dag_to_graphviz(node_to_visualize_after_exit.dag)
 
-        genesis_creation_time = int(datetime.datetime.now().timestamp() - BLOCK_TIME) #so we start right from the first block
-
-        private_keys = BlockSigners()
-
-        network = NodeApi()
-
-        tasks = []
-
-        logger = logging.getLogger("Announce")
-        announcer = AnnouncerNode(genesis_creation_time, logger)
-        tasks.append(announcer.run())
-         
-        for i in range(0, 19):
-            behaviour = Behaviour()
-            logger = logging.getLogger("Node " + str(i))
-            # uncomment the following line to enable logging only on specific node
-            # if i != 13: logger.setLevel(logging.CRITICAL)
-            node = Node(genesis_creation_time, i, network, logger, private_keys.block_signers[i], behaviour)
-
-            if i == 0:
-                global node_to_visualize
-                node_to_visualize = node
-            network.register_node(node)
-            tasks.append(node.run())
-
-        for i in range(19, 20):
-            behaviour = Behaviour()
-            behaviour.wants_to_hold_stake = True
-            behaviour.epoch_to_release_stake = 2
-            logger = logging.getLogger("Node " + str(i))
-            keyless_node = Node(genesis_creation_time, i, network, logger, None, behaviour)
-            network.register_node(keyless_node)
-            tasks.append(keyless_node.run())
-
-        signal.signal(signal.SIGINT, signal_handler)
-
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(asyncio.gather(*tasks))       
-        loop.close()
         
 Initializer()
