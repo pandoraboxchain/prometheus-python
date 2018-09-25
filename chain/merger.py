b7e51db7ebc6a94d7ce026ada7ac04bbd5afb374
Julian Konchunas <konchunas@gmail.com>
2018-09-25 19:48:19 +0300

    Fix merger indentation and add merging code prototype

--- chain/merger.py
+++ chain/merger.py
@@ -32,17 +32,13 @@ class Merger:
             top = top_blocks[0]
 
         return top, conflicts
-            top = top_blocks[0]
 
-        return top, conflicts
-
-    #determines hash of common ancestor of two chains using first-only children iteration approach
     def get_common_ancestor(self, first_chain, second_chain):
         first_blocks = []
         second_blocks = []
         first_iter = ChainIter(self.dag, first_chain)
         second_iter = ChainIter(self.dag, second_chain)
-        while True: #TODO some sane alternative algorithm
+        while True: #TODO sane exit condition
             first_chain_block = first_iter.next()
             if first_chain_block:
                 block_hash = first_chain_block.get_hash()
@@ -55,3 +51,68 @@ class Merger:
                 if block_hash in first_blocks:
                     return block_hash
                 second_blocks.append(block_hash)
+
+    #returns part of of second chain where they diverge
+    def get_difference(self, first_chain, second_chain):
+        common_ancestor = self.get_common_ancestor(first_chain, second_chain)
+
+        chain_diff = []
+        chain_iter = ChainIter(self.dag, second_chain)
+        for block in chain_iter:
+            if block:
+                if block.get_hash() == common_ancestor:
+                    break
+            
+            if block:
+                chain_diff.append(block)
+            else:
+                chain_diff.append(None)
+
+        return chain_diff
+        
+    def merge(self):
+        tops = self.dag.get_top_blocks_hashes()
+        sizes = [self.dag.calculate_chain_length(top) for top in tops]
+        dict_sizes = dict(enumerate(sizes))
+        deterministic_ordering = []
+        while dict_sizes:
+            m = max(dict_sizes.values())
+            indexes = [key for key,value in dict_sizes.items() if value==m]
+            if len(indexes) == 1:
+                dict_sizes.pop(indexes[0])
+                deterministic_ordering.append(indexes[0])
+            else:
+                for item in indexes:
+                    dict_sizes.pop(item)
+                random.shuffle(indexes)
+                deterministic_ordering += indexes
+
+        longest_chain = tops[deterministic_ordering[0]]
+        immutability = Immutability(self.dag)
+        longest_chain_mutable_part = immutability.get_mutable_part_of_chain(longest_chain)
+        # active_merged_point = Chain(active[:merging_point])
+        merged_chain = []
+
+        for doi in deterministic_ordering[1:]:
+            diffchain = self.get_difference(longest_chain_mutable_part, tops[doi])
+            im_chain = diffchain.get_all_immutable()
+            if im_chain:
+                for im in im_chain:
+                    if not im.is_empty:
+                        if not merged_chain.find_block_by_identifier(im.identifier):
+                            merged_chain.append(im)
+        
+        for doi in deterministic_ordering:
+            diffchain = active_merged_point.get_diff(chains[doi])[1]
+            m_chain = diffchain.get_all_mutable()
+            if m_chain:
+                for m in m_chain:
+                    if not m.is_empty:
+                        if not merged_chain.find_block_by_identifier(m.identifier):
+                            merged_chain.append(m)
+
+        return {
+            "deterministic_ordering": deterministic_ordering,
+            "merged_chain": merged_chain,
+        }
+
