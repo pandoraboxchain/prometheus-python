26d9ab59f800e11129bb0161309ed22f519acdaa
Julian Konchunas <konchunas@gmail.com>
2018-10-02 21:30:34 +0300

    Add complex merge test

--- tests/merger.py
+++ tests/merger.py
@@ -63,6 +63,29 @@ class TestMerger(unittest.TestCase):
         self.assertEqual(res[2], dag.blocks_by_number[2][0])
         self.assertEqual(res[3], dag.blocks_by_number[3][0])
 
+    def test_complex_merge(self):
+        dag = Dag(0)
+        genesis_hash = dag.genesis_block().get_hash()
+        TestChainGenerator.fill_with_dummies_and_skips(dag, genesis_hash, range(1,10), [2,5,7,8])
+        first_block = dag.blocks_by_number[1][0].get_hash()
+        TestChainGenerator.fill_with_dummies_and_skips(dag, first_block, range(2,10), [3,4,6,7,8,9])
+        second_block = dag.blocks_by_number[2][0].get_hash()
+        TestChainGenerator.fill_with_dummies_and_skips(dag, second_block, range(3,10), [3,4,5,6,9])
+        
+        merger = Merger(dag)
+        res = merger.merge()
+
+        self.assertEqual(res[0], dag.blocks_by_number[0][0])
+        self.assertEqual(res[1], dag.blocks_by_number[1][0])
+        self.assertEqual(res[2], dag.blocks_by_number[3][0])
+        self.assertEqual(res[3], dag.blocks_by_number[4][0])
+        self.assertEqual(res[4], dag.blocks_by_number[6][0])
+        self.assertEqual(res[5], dag.blocks_by_number[9][0])
+        self.assertEqual(res[6], dag.blocks_by_number[2][0]) #TODO find out why is this 2 here
+        self.assertEqual(res[7], dag.blocks_by_number[7][0])
+        self.assertEqual(res[8], dag.blocks_by_number[8][0])
+        self.assertEqual(res[9], dag.blocks_by_number[5][0])
+
     def test_two_tops_on_epoch_end(self):
         # generate two blocks on epoch end
         # 1 --- [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
