5363cf29622fefe77423a28f4e9ee246249d0d5f
Strider <striderbox@gmail.com>
2018-10-15 10:28:00 +0300

    small fix test launcher

--- runtest.py
+++ runtest.py
@@ -1,18 +1,19 @@
 import unittest
 
-from tests.block import *
-from tests.stake_transaction import *
-from tests.transaction import *
-from tests.epoch import *
-from tests.secret_sharing import *
-from tests.dag import *
-from tests.merger import *
-from tests.permissions import *
+from tests.test_block import *
+from tests.test_chain_generator import *
+from tests.test_confirmation_requirement import *
+from tests.test_dag import *
+from tests.test_epoch import *
 from tests.test_gossip import *
-from tests.random import *
-from tests.immutability import *
-from tests.node import *
-from tests.merging_iterator import *
-from tests.confirmation_requirement import *
+from tests.test_immutability import *
+from tests.test_merger import *
+from tests.test_merging_iterator import *
+from tests.test_node import *
+from tests.test_permissions import *
+from tests.test_random import *
+from tests.test_secret_sharing import *
+from tests.test_stake_transaction import *
+from tests.test_transaction import *
 
 unittest.main()
