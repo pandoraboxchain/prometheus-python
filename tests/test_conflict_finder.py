8742993d6bf005030899858cb6edc20283707f5c
Strider <striderbox@gmail.com>
2018-10-17 12:13:59 +0300

    add match more complicated test

--- tests/test_conflict_finder.py
+++ tests/test_conflict_finder.py
@@ -141,7 +141,7 @@ class TestConflictFinder(unittest.TestCase):
         # conflicts include all [3,3],[4,4],[5,5],[6,6,6],[7,7],[8,8] excluding!!! determined top (one of 8)
         self.assertEqual(len(conflicts), 13)
 
-    def test_complicated_chain_with_skips(self):
+    def test_complicated_dag_with_skips(self):
         dag = Dag(0)
         # generate test case
         # time_slot [0, 1, 2, 3, 4, 5]
@@ -186,14 +186,77 @@ class TestConflictFinder(unittest.TestCase):
                                                            dummy_private=private4)
 
         DagVisualizer.visualize(dag)
+
         conflict_finder = ConflictFinder(dag)
         top_blocks = list(dag.get_top_blocks().keys())
         top, conflicts = conflict_finder.find_conflicts(top_blocks)
-        # assert determined top (it can be one of top_hash1,2,3)
+        # assert determined top (it can be one of top_hash_1,2,3,4)
         tops = [top_hash_1, top_hash_2, top_hash_3, top_hash_4]
         self.assertIn(top, tops)
         # test conflicts
-        # conflicts include all [2,2],[3,3],[4,4,4],[5,5,4],[6,6,6],[7,7],[8,8,8] excluding!!! determined top (one of 8)
+        # conflicts include all [2,2],[3,3],[4,4,4],[5,5,5],[6,6,6],[7,7],[8,8,8] excluding!!! determined top (one of 8)
         self.assertEqual(len(conflicts), 18)
+
+    def test_complicated_dag_with_skips_and_determined_top(self):
+        dag = Dag(0)
+        # generate test case
+        # time_slot [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        # ----------------------------------------
+        # 1 ------- [ ,  , 2, 3, 4,  ,  ,  , 8,  ]
+        # 2 ------- [0, 1, 2,  ,  , 5, 6, 7, 8, 9]
+        # 3 ------- [ ,  ,  , 3, 4,  , 6, 7, 8,  ]
+        # 4 ------- [ ,  ,  ,  , 4, 5, 6,  , 8,  ]
+        # 5 ------- [ ,  , 2, 3,  ,  ,  ,  ,  , 9]
+        # block number 3 MUST BE signed by same key
+        private1 = Private.generate()
+        private2 = Private.generate()
+        private3 = Private.generate()
+        private4 = Private.generate()
+        private5 = Private.generate()
+
+        top_hash_2 = \
+            TestChainGenerator.fill_with_dummies_and_skips(dag=dag,
+                                                           prev_hash=dag.genesis_block().get_hash(),
+                                                           range=range(1, 11, 1),
+                                                           indices_to_skip=[3, 4, 10],
+                                                           dummy_private=private2)
+
+        top_hash_1 = \
+            TestChainGenerator.fill_with_dummies_and_skips(dag=dag,
+                                                           prev_hash=dag.blocks_by_number[1][0].get_hash(),
+                                                           range=range(2, 11, 1),
+                                                           indices_to_skip=[6, 7],
+                                                           dummy_private=private1)
+
+        top_hash_3 = \
+            TestChainGenerator.fill_with_dummies_and_skips(dag=dag,
+                                                           prev_hash=dag.blocks_by_number[2][0].get_hash(),
+                                                           range=range(3, 11, 1),
+                                                           indices_to_skip=[5, 7, 10],
+                                                           dummy_private=private3)
+
+        top_hash_4 = \
+            TestChainGenerator.fill_with_dummies_and_skips(dag=dag,
+                                                           prev_hash=dag.blocks_by_number[3][1].get_hash(),
+                                                           range=range(4, 11, 1),
+                                                           indices_to_skip=[5, 7, 10],
+                                                           dummy_private=private4)
+
+        top_hash_5 = \
+            TestChainGenerator.fill_with_dummies_and_skips(dag=dag,
+                                                           prev_hash=dag.blocks_by_number[3][1].get_hash(),
+                                                           range=range(4, 11, 1),
+                                                           indices_to_skip=[4, 5, 6, 7, 8],
+                                                           dummy_private=private5)
+
+        DagVisualizer.visualize(dag)
+
+        conflict_finder = ConflictFinder(dag)
+        top_blocks = list(dag.get_top_blocks().keys())
+        top, conflicts = conflict_finder.find_conflicts(top_blocks)
+        self.assertEqual(top, top_hash_1)  # strong determined top (longest chain in current case) min. skipped blocks
+        # test conflicts
+        # conflicts include all [2,2],[3,3],[4,4,4],[5,5],[6,6,6],[7],[8,8,8,8],[9,9,9,9,9],[10]
+        self.assertEqual(len(conflicts), 23)
 
 
