60579f8a523aa36843c0d3e5e42a3480b37ac02e
Julian Konchunas <konchunas@gmail.com>
2018-10-19 22:13:53 +0300

    Test merged chain with transaction conflicts

--- tests/test_merge_and_resolve.py
+++ tests/test_merge_and_resolve.py
@@ -33,9 +33,7 @@ class TestMergeAndResolve(unittest.TestCase):
         self.assertIn(Entry(block2_reward.get_hash(), 0), unspent)
         self.assertIn(Entry(payment.get_hash(), 0), unspent)
 
-    def test_merged(self):
-        """Test merged chain but non confilcting transactions
-        """
+    def test_merged_chain_without_transaction_conflicts(self):
         dag = Dag(0)
 
         block_hash, block_reward = TestChainGenerator.insert_dummy_with_payments(dag, [dag.genesis_hash()], [], 1)
@@ -62,6 +60,38 @@ class TestMergeAndResolve(unittest.TestCase):
         self.assertIn(Entry(block4_reward, 0), unspent)
         self.assertIn(Entry(payment1.get_hash(), 0), unspent)
         self.assertIn(Entry(payment2.get_hash(), 0), unspent)
+
+    def test_merged_chain_with_transaction_conflicts(self):
+        dag = Dag(0)
+
+        block_hash, block_reward = TestChainGenerator.insert_dummy_with_payments(dag, [dag.genesis_hash()], [], 1)
+
+        #second block spends first block reward
+        payment1 = TransactionFactory.create_payment(block_reward, 0, [os.urandom(32)], [15])
+        block2_hash, block2_reward = TestChainGenerator.insert_dummy_with_payments(dag, [block_hash], [payment1], 2)
+
+        #third block also spends first block reward
+        payment2 = TransactionFactory.create_payment(block_reward, 0, [os.urandom(32)], [15])
+        block3_hash, block3_reward = TestChainGenerator.insert_dummy_with_payments(dag, [block_hash], [payment2], 3)
+
+        #making block2_hash go first, so its transactions will have a priority
+        block4_hash, block4_reward = TestChainGenerator.insert_dummy_with_payments(dag, [block2_hash, block3_hash], [], 4)
+
+        iterator = MergingIter(dag, block4_hash)
+        payments = [block.block.payment_txs for block in iterator if block != None]
+        payments = list(reversed(payments))
+
+        spent, unspent = Resolver.resolve(payments)
+
+        self.assertEqual(len(spent), 0)
+
+        self.assertEqual(len(unspent), 4)
+
+        self.assertIn(Entry(block2_reward, 0), unspent)
+        self.assertIn(Entry(block3_reward, 0), unspent)
+        self.assertIn(Entry(block4_reward, 0), unspent)
+        self.assertIn(Entry(payment1.get_hash(), 0), unspent)
+        self.assertNotIn(Entry(payment2.get_hash(), 0), unspent) #payment 2 is consedered conflicting as it goes later in merged chain
 
 
 
