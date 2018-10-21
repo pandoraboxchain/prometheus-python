34b6ea187de43fcd64934990ebd21f15198f9b45
Julian Konchunas <konchunas@gmail.com>
2018-10-22 02:40:33 +0300

    Test both explicit and candidate conflicts search in Conflict Watcher

--- tests/test_conflict_watcher.py
+++ tests/test_conflict_watcher.py
@@ -179,6 +179,7 @@ class TestConflictWatcher(unittest.TestCase):
 
         actor2 = Private.publickey(Private.generate())
 
+        #consider common ancestor already resolved
         block1_hash = TestChainGenerator.insert_dummy(dag, [dag.genesis_hash()], 1)
         watcher.on_new_block_by_validator(block1_hash, 1, actor2)
         
@@ -191,13 +192,47 @@ class TestConflictWatcher(unittest.TestCase):
         tops = dag.get_top_hashes()
         common_ancestor = dag.get_common_ancestor(tops)
 
-        #here block was signed by node even before merge appeared
-        #this is explicit merge and both following blocks are conflicting
         explicits, candidates = watcher.find_conflicts_in_between(tops, common_ancestor)
         self.assertEqual(len(explicits), 2)
         self.assertEqual(len(candidates), 0)
         self.assertIn(block2_hash, explicits)
         self.assertIn(block2c_hash, explicits)
+
+    def test_both_types_of_conflicts(self):
+        dag = Dag(0)
+        watcher = ConflictWatcher(dag)
+
+        actor1 = Private.publickey(Private.generate())
+        actor2 = Private.publickey(Private.generate())
+        actor3 = Private.publickey(Private.generate())
+
+        block1_hash = TestChainGenerator.insert_dummy(dag, [dag.genesis_hash()], 1)
+        watcher.on_new_block_by_validator(block1_hash, 1, actor1)
+        
+        block2_hash = TestChainGenerator.insert_dummy(dag, [block1_hash], 2)
+        watcher.on_new_block_by_validator(block2_hash, 1, actor2)
+
+        block2c_hash = TestChainGenerator.insert_dummy(dag, [block1_hash], 2)
+        watcher.on_new_block_by_validator(block2c_hash, 1, actor2)
+
+        block3_hash = TestChainGenerator.insert_dummy(dag, [block2_hash], 3)
+        watcher.on_new_block_by_validator(block3_hash, 1, actor3)
+
+        #this is possible if we have two epoch seeds
+        block4_hash = TestChainGenerator.insert_dummy(dag, [block2c_hash], 4)
+        watcher.on_new_block_by_validator(block4_hash, 1, actor1)
+
+        tops = dag.get_top_hashes()
+        common_ancestor = dag.get_common_ancestor(tops)
+
+        explicits, candidate_groups = watcher.find_conflicts_in_between(tops, common_ancestor)
+        self.assertEqual(len(explicits), 1)
+        self.assertIn(block4_hash, explicits)
+
+        self.assertEqual(len(candidate_groups), 1)
+        self.assertEqual(len(candidate_groups[0]), 2)
+        self.assertIn(block2_hash, candidate_groups[0])
+        self.assertIn(block2c_hash, candidate_groups[0])
 
 
 
